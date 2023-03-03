f43750b1678dbfec0f7cea4fce628960e83334ce
bahauddin nafis <bahauddinafis@gmail.com>
2023-03-03 16:13:25 +0700

    belajar membuat website kedai kopi artaloka kopi

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Toggle class active pada hamburger-menu
+const navbarNav = document.querySelector(".navbar-nav");
+// Ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Klik di luar sidebar untuk menghilangkan navbar-nav
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
